8c9e559c7060e83e7025a86bc500b1305f1d4c04
Bikash Chauhan <chauhanbikash789@gmail.com>
2024-03-04 19:31:59 +0530

    Bugfix: Update the docker build image to add the image tag

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@ def call(String imageName, String tagName)
 {
     echo 'Building the docker images...'
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]) {
-        sh("docker build -t $imageName .")
+        sh("docker build -t $imageName:$tagName .")
         sh("echo $PASS | docker login -u $USERNAME --password-stdin")
         sh("docker push $imageName:$tagName")
     }
