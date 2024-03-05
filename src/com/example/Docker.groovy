24eb4148c68926168143613a81cf414cd96ab71e
Bikash Chauhan <chauhanbikash789@gmail.com>
2024-03-05 14:03:43 +0530

    Update: Fixed security warnings

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,18 +10,18 @@ class Docker implements Serializable{
 
     def buildDockerImage(String imageName, String tagName) {
         script.echo 'Building the docker image...'
-        script.sh("docker build -t $imageName:$tagName .")
+        script.sh('docker build -t $imageName:$tagName .')
     }
 
     def dockerLogin()
     {
         script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]) {
-            script.sh("echo $script.PASS | docker login -u $script.USERNAME --password-stdin")
+            script.sh('echo $script.PASS | docker login -u $script.USERNAME --password-stdin')
         }
     }
 
     def dockerPush(String imageName, String tagName)
     {
-        script.sh("docker push $imageName:$tagName")
+        script.sh('docker push $imageName:$tagName')
     }
 }
